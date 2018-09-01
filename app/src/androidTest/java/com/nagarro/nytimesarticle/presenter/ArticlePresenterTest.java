d5dab7b58bd3f8e505dc0fd906a624263afc7cd6
NAGARRO\gaganpreetsingh <gaganpreet.singh@nagarro.com>
2018-09-01 17:27:00 +0530

    Added test cases

--- app/src/androidTest/java/com/nagarro/nytimesarticle/presenter/ArticlePresenterTest.java
+++ app/src/androidTest/java/com/nagarro/nytimesarticle/presenter/ArticlePresenterTest.java
@@ -1,27 +1,74 @@
 package com.nagarro.nytimesarticle.presenter;
 
+import android.content.Context;
+
+import com.nagarro.nytimesarticle.network.ErrorType;
+import com.nagarro.nytimesarticle.view.IBaseView;
+
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.junit.MockitoRule;
 
 /**
- * Article presenter test
+ * UTC for Article presenter
  */
+@RunWith(MockitoJUnitRunner.class)
 public class ArticlePresenterTest {
+
+    @Rule
+    public MockitoRule mMockitoRule = MockitoJUnit.rule();
+    @Mock
+    private IBaseView mView;
+    @Mock
+    private Context mContext;
+
+    private ArticlePresenter mPresenter;
 
     @Before
     public void setUp() throws Exception {
+        mPresenter = new ArticlePresenter(mContext, mView);
     }
 
     @Test
-    public void getArticleListObserver() {
+    public void getArticleList_success() {
+        mView.showProgressDialog();
+        mPresenter.getArticleList();
+        mView.hideProgressDialog();
     }
 
     @Test
-    public void getArticleList() {
+    public void getArticleList_failure_no_network() {
+        mView.showProgressDialog();
+        mPresenter.getArticleList();
+        mView.hideProgressDialog();
+        mView.showError(ErrorType.NO_NETWORK, "Network not available!");
+    }
+
+    @Test
+    public void getArticleList_failure_bad_response() {
+        mView.showProgressDialog();
+        mPresenter.getArticleList();
+        mView.hideProgressDialog();
+        mView.showError(ErrorType.BAD_RESPONSE, "Bad network response!");
+    }
+
+    @Test
+    public void getArticleList_failure_server_timeout() {
+        mView.showProgressDialog();
+        mPresenter.getArticleList();
+        mView.hideProgressDialog();
+        mView.showError(ErrorType.SERVER_TIMEOUT, "Server response timeout!");
     }
 
     @After
     public void tearDown() throws Exception {
+        mPresenter.dispose();
+        mPresenter = null;
     }
 }
