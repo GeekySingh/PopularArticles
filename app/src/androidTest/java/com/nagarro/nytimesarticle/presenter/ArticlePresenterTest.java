4c65c6fcdee28f4fa18e2d8c1a55cb7d4500134e
NAGARRO\gaganpreetsingh <gaganpreet.singh@nagarro.com>
2018-09-01 15:57:30 +0530

    added presenter test case

---
+++ app/src/androidTest/java/com/nagarro/nytimesarticle/presenter/ArticlePresenterTest.java
@@ -0,0 +1,27 @@
+package com.nagarro.nytimesarticle.presenter;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Article presenter test
+ */
+public class ArticlePresenterTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void getArticleListObserver() {
+    }
+
+    @Test
+    public void getArticleList() {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+}
